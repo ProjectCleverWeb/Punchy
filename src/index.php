7ca0f7ffe264a0524482cf24852a7e82a9c9a0f3
Nicholas Jordon <projectcleverweb@gmail.com>
2014-10-07 19:42:49 -0400

    removed the index page redundancy between /src/index.php and /src/public/index.php

--- src/index.php
+++ src/index.php
@@ -1,4 +1,3 @@
 <?php
 
-require_once realpath(__DIR__.'/system/autoload.php');
-projectcleverweb\punchy\main::init(require_once(realpath(__DIR__.'/config.php')));
+require_once realpath(__DIR__.'/public/index.php');
