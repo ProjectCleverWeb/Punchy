4e95a92db93d1980f8f4edbbe5f2e458f3b37d71
Nicholas Jordon <projectcleverweb@gmail.com>
2014-10-06 22:15:41 -0400

    added unit testing and continuous integration components

---
+++ testing/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Bootstrap
+ */
+
+namespace projectcleverweb\punchy;
+
+// Call the autoload script
+require_once realpath(__DIR__.'/../autoload.php');
+
+// Do something
---
+++ testing/tests/00_Configuration.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace projectcleverweb\punchy;
+
+/**
+ * @requires PHP 5.4
+ * @requires PHPUnit 4
+ */
+class ConfigTest extends \PHPUnit_Framework_TestCase {
+	
+	/**
+	 * @test
+	 */
+	public function True() {
+		$this->assertTrue(TRUE);
+	}
+}
